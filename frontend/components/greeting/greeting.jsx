7ecf27c316bc604a6f0f9b7f2b2839f4f50d283e
ishafran <isabelle.shafran@gmail.com>
2020-12-07 11:57:13 -0800

    add greeting container and component

---
+++ frontend/components/greeting/greeting.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Greeting = ({ currentUser, logout }) => {
+    const sessionLinks = () => (
+        <nav>
+            <Link to={"/login"}>Sign in</Link>
+            <Link to={"/signup"}>Create an account</Link>
+        </nav>
+    )
+
+    const peronsalGreeting = () => (
+        <hgroup>
+            <h2>Welcome {currentUser.first_name}</h2>
+            <button onClick={logout}>Sign Out</button>
+        </hgroup>
+    )
+
+    return currentUser ? peronsalGreeting() : sessionLinks();
+}
+
+export default Greeting;
---
+++ frontend/components/greeting/greeting_container.js
@@ -0,0 +1,13 @@
+import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
+import Greeting from './greeting';
+
+const msp = ({session, entities: {users} }) => ({
+    currentUser: users[session.id]
+})
+
+const mdp = (dispatch) => ({
+    logout: () => dispatch(logout())
+})
+
+export default connect(msp, mdp)(Greeting)

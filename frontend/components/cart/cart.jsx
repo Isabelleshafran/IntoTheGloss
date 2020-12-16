6a2cb4eb47444063925a184b6e955134f56d4e7c
ishafran <isabelle.shafran@gmail.com>
2020-12-16 15:30:09 -0800

    cart update

--- frontend/components/cart/cart.jsx
+++ frontend/components/cart/cart.jsx
@@ -29,59 +29,46 @@ class Cart extends React.Component {
             localStorage.setItem('cartObj', JSON.stringify(items))
         }
 
-        this.props.openModal('account')
+        if(!this.props.currentUser) {
+            this.props.openModal('signup')
+        } else {
+            this.props.openModal('account')
+        }
     }
     
-    render() { 
-        if(!this.props.currentUser) {
-            return (
-                <div>
-                    <div className="no-user-cart">
-                        <div onClick={this.props.closeModal} className="x">X</div>
-                        <div className="no-user-header">Please sign in to view or add to your cart</div>
-                    </div>
-                    <div className='bottom-cart'>
-                        <img className="glogo" src={window.shopping_cart_g} />
-                        <div className="create-account-button-cart" onClick={() => this.props.openModal('signup')}>Create an Account Here</div>
-                    </div>
+render() { 
+        return (
+            <div>
+                <div className="your-bag-header">
+                    <div onClick={this.props.closeModal} className="x">X</div>
+                    <div className="your-bag">Your Bag: </div>
                 </div>
-            )
-        } else {
-    
-            return (
-                <div>
-                    <div className="your-bag-header">
-                        <div onClick={this.props.closeModal} className="x">X</div>
-                        <div className="your-bag">Your Bag: </div>
-                    </div>
-                    <br/>
+                <br/>
 
-                    <div className="cart">
-                        {this.StoragePull().map(product => {
-                            return (
-                                <div>
-                                    <CartItem product={product} 
-                                    key={product.title}
-                                    openModal={this.props.openModal}
-                                    closeModal={this.props.closeModal}/>
-                                </div>
-                            )
-                        })}
+                <div className="cart">
+                    {this.StoragePull().map(product => {
+                        return (
+                            <div>
+                                <CartItem product={product} 
+                                key={product.title}
+                                openModal={this.props.openModal}
+                                closeModal={this.props.closeModal}/>
+                            </div>
+                        )
+                    })}
 
-                    </div>
-                    <div>
-                        <div className="cart-total">
-                            <div className="estimated-total">Estimated Total: </div>
-                            <div>${this.CartTotal()}</div>
-                        </div>
-
-                    </div>
-
-                    <button className="check-out" onClick={() => this.handleCheckOut()}>Check Out</button>
                 </div>
-            )
-        }
+                <div>
+                    <div className="cart-total">
+                        <div className="estimated-total">Estimated Total: </div>
+                        <div>${this.CartTotal()}</div>
+                    </div>
 
+                </div>
+
+                <button className="check-out" onClick={() => this.handleCheckOut()}>Check Out</button>
+            </div>
+        )
     }
 }
  

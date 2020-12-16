2cb6e7cc1f476475023e691a9f35841d8f500461
ishafran <isabelle.shafran@gmail.com>
2020-12-16 15:38:12 -0800

    clear cart

--- frontend/components/header/account.jsx
+++ frontend/components/header/account.jsx
@@ -14,6 +14,7 @@ class Account extends React.Component {
     }
 
     handleClick() {
+        localStorage.setItem(('cartObj'), '{}')
         this.props.logout()
         this.props.openModal('signin')
     }
